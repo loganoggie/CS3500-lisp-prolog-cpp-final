438a4297b5db39d5e012c974ef7f7f5feb2c4bd1
Logan Nielsen <loganoggie@gmail.com>
2017-12-12 20:57:30 -0600

    stuff

--- thing.pl
+++ thing.pl
@@ -2,3 +2,11 @@
     Logan Nielsen
     Exam 3 - problem 15 - part (b)
 */
+
+isDivisible(I) :- 0 is mod(I, 3).
+isDivisible(I) :- 0 is mod(I, 5).
+isDivisible(I) :- 0 is mod(I, 7).
+
+addMods(I, Sum) :- I > 1000, write(Sum).
+addMods(I, Sum) :- I =< 1000, isDivisible(I), TmpSum is Sum+I, TmpI is I+1, addMods(TmpI, TmpSum).
+addMods(I, Sum) :- I =< 1000, \+ isDivisible(I), TmpI is I+1, addMods(TmpI, Sum).
